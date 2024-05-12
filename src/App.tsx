65740dfd087189ae654f039cdfd69202acc248a4
leeJeonghyunz <dlwjdus3217@naver.com>
2024-05-12 23:19:13 +0900

    cleanApp

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
           <Search />
         </Route>
         <Route path={["/", "/movies/movieId"]}>
-          {/* react-router가 두개의 path에서 같은 컴포넌트를 render 하도록 한다. */}
+          {/* react-router가 두개의 path에서 같은 컴포넌트를 render 하도록 한다.. */}
           {/* // 코드에서 "/" 경로를 맨 아래에 두는 이유는 리액트 라우터가 먼저 매칭되는 경로를 선택하기 때문에, 다른 경로가 먼저 처리되도록 보장하기 위함. */}
           <Home />
         </Route>
